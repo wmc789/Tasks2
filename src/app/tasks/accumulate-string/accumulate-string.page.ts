9b396975ff73c3c606feb7db3f97f43a296cb8f2
wmc789 <w.burk1@web.de>
2022-06-21 08:55:11 +0200

    opened new brnach and added ion Card

--- src/app/tasks/accumulate-string/accumulate-string.page.ts
+++ src/app/tasks/accumulate-string/accumulate-string.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input , Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-accumulate-string',
@@ -7,9 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AccumulateStringPage implements OnInit {
 
+  
   //private myString = '';
   inputString = '';
   outputString = '';
+
+  exampleString = 'abcd';
 
   constructor() { }
 
